489918a422f3bbda4fdb80fa7f549c81e383d949
RainHu6665 <58576696+RainHu6665@users.noreply.github.com>
2019-12-09 17:21:42 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,14 +81,14 @@ public class Robot extends TimedRobot {
     UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture();
     UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture();
     
-  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tx = table.getEntry("tx");
-  NetworkTableEntry ty = table.getEntry("ty"); 
-  NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty"); 
+    NetworkTableEntry ta = table.getEntry("ta");
 
-  double x = tx.getDouble(0.0);
-  double y = ty.getDouble(0.0);
-  double area = ta.getDouble(0.0);
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
 
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
